aab9bc2ea8f30364479cdbf8ec0176c9fbc217b8
linomp <lm2p@hotmail.es>
2024-01-26 19:44:56 +0100

    Update FE client

--- mvp/client/src/generated/index.ts
+++ mvp/client/src/generated/index.ts
@@ -8,11 +8,13 @@ export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { GameSessionDTO } from './models/GameSessionDTO';
+export type { GlobalSettings } from './models/GlobalSettings';
 export type { HTTPValidationError } from './models/HTTPValidationError';
 export type { MachineState } from './models/MachineState';
 export type { OperationalParameters } from './models/OperationalParameters';
 export type { ValidationError } from './models/ValidationError';
 
 export { DefaultService } from './services/DefaultService';
+export { GlobalSettingsService } from './services/GlobalSettingsService';
 export { MachineInterventionsService } from './services/MachineInterventionsService';
 export { SessionsService } from './services/SessionsService';
