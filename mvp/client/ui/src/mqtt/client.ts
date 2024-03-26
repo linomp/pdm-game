7a245379e5749e03e06d69bc9749692c7374179c
linomp <lm2p@hotmail.es>
2024-03-27 00:04:25 +0100

    add working mqtt example client & server

--- mvp/client/ui/src/mqtt/client.ts
+++ mvp/client/ui/src/mqtt/client.ts
@@ -2,18 +2,34 @@ import mqtt from "mqtt";
 
 export const getClient = async (): Promise<mqtt.MqttClient> => {
 
-    const brokerUrl = "wss://test.mosquitto.org:8081/mqtt";//import.meta.env.VITE_MQTT_BROKER_URL;
+    const brokerUrl = import.meta.env.VITE_MQTT_HOST;
 
     try {
-        const client = await mqtt.connectAsync(brokerUrl);
+
+        const client = await mqtt.connectAsync(
+            brokerUrl,
+            {
+                username: import.meta.env.VITE_MQTT_USERNAME,
+                password: import.meta.env.VITE_MQTT_PASSWORD
+            }
+        );
+
         if (import.meta.env.VITE_DEBUG) {
             console.log("Connected to MQTT broker at: ", brokerUrl);
-            client.subscribe("pdmgame/clients/test");
+            client.subscribe(`${import.meta.env.VITE_MQTT_TOPIC_PREFIX}/test`,
+                (err) => {
+                    if (err) {
+                        console.error("Error subscribing to topic: ", err);
+                    }
+                }
+            );
             client.on("message", (topic, message) => {
                 console.log(`Received message on topic ${topic}: ${message.toString()}`);
             });
         }
+
         return client;
+
     } catch (error) {
         console.error(`Error connecting to MQTT broker ${brokerUrl}: `, error);
         throw error;
