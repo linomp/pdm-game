c13cd85b80230e5123979cf3e768561fcfa8e279
linomp <linomp95@hotmail.com>
2024-07-17 00:31:26 +0200

    Update load test script

--- mvp/server/tests/locustfile.py
+++ mvp/server/tests/locustfile.py
@@ -9,44 +9,35 @@ class ApiUser(HttpUser):
     @task
     def advance_turn(self):
         if self.is_game_over:
+            self.stop()
             return
 
         if self.session_id is None:
             # Create a session
             with self.client.post("/sessions/", catch_response=True) as response:
                 try:
-                    if response.status_code == 200:
-                        self.session_id = response.json()["id"]
-                    else:
-                        raise Exception
+                    self.session_id = response.json()["id"]
                 except Exception:
                     response.failure("Failed to create session")
                     print(str(response.content))
-                    self.stop()
                     return
 
         # Advance the turn
         with self.client.put(f"/sessions/turns?session_id={self.session_id}", catch_response=True) as response:
-            if response.status_code == 200:
-                try:
-                    json_response = response.json()
-                    if "is_game_over" in json_response and json_response["is_game_over"]:
-                        self.is_game_over = True
-                        print(f"Reached game over for session {self.session_id}")
-                        response.success()
-                        self.stop()
-                    elif "id" in json_response and "current_step" in json_response:
-                        response.success()
-                    else:
-                        raise Exception
-                except Exception:
-                    response.failure("Unexpected response structure")
-                    print(str(response.content))
+            try:
+                json_response = response.json()
+                if "is_game_over" in json_response and json_response["is_game_over"]:
+                    self.is_game_over = True
+                    print(f"Reached game over for session {self.session_id}")
+                    response.success()
                     self.stop()
-            else:
+                elif "id" in json_response and "current_step" in json_response:
+                    response.success()
+                else:
+                    raise Exception
+            except Exception:
                 response.failure("Failed to advance turn")
                 print(str(response.content))
-                self.stop()
 
 
 if __name__ == "__main__":
